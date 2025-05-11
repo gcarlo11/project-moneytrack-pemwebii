0b6ffe38a24e3f765a6de68257561ddb975b19dc
gcarlo11 <ridhorasyid676@gmail.com>
2025-05-11 14:25:04 +0700

    Set: Model for Category and Transaction

---
+++ app/Models/Transaction.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['category_id', 'amount', 'description', 'transaction_date'];
+    
+    protected $casts = [
+        'transaction_date' => 'date',
+        'amount' => 'decimal:2',
+    ];
+    
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
